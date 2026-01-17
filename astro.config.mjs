3cd7c416b6f44b6c49e0a4bf3e4f89136cb8a3cf
Leandro Afonso <leo@0x1eo.dev>
2026-01-17 23:36:57 +0000

    Fix: deploy to /packrat subfolder

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://blog.0x1eo.dev',
+	base: '/packrat',
 	integrations: [mdx(), sitemap()],
 });
