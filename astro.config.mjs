3a6b094464ec892b09eb9d2e5ecfc79e4c7cf9e5
Leandro Afonso <leo@0x1eo.dev>
2026-01-17 23:34:50 +0000

    Remove /packrat base path for root deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://blog.0x1eo.dev',
-	base: '/packrat',
 	integrations: [mdx(), sitemap()],
 });
